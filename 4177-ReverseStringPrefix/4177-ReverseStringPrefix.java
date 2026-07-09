3e2ed1ce4bd8d676cb855cc9513cff9c5b7ab41f
Ashwath <achukavii361@gmail.com>
2026-07-09 15:05:56 +0530

    Create 4177-ReverseStringPrefix

---
+++ 4177-ReverseStringPrefix/4177-ReverseStringPrefix.java
@@ -0,0 +1,16 @@
+// Last updated: 7/9/2026, 3:05:55 PM
+class Solution {
+    public String reversePrefix(String s, int k) {
+        char[] arr = s.toCharArray();
+        int left = 0;
+        int right = k - 1;
+        while (left < right) {
+            char temp = arr[left];
+            arr[left] = arr[right];
+            arr[right] = temp;
+            left++;
+            right--;
+        }
+        return new String(arr);
+    }
+}
