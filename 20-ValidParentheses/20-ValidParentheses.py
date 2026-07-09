9731b0070cb3f672f15123ab3bc62c87d163e519
Ashwath <achukavii361@gmail.com>
2026-07-09 15:11:11 +0530

    Create 20-ValidParentheses

---
+++ 20-ValidParentheses/20-ValidParentheses.py
@@ -0,0 +1,22 @@
+# Last updated: 7/9/2026, 3:11:10 PM
+class Solution:
+    def isValid(self, s: str) -> bool:
+        i=0
+        a=[]
+        for i in range(len(s)):
+            if s[i]=='('or s[i]=='['or s[i]=='{':
+                a.append(s[i])
+            else:
+                if not a:
+                    return False
+                top=a.pop()
+                if s[i]==')'and top!='(':
+                    return False
+                if s[i]==']'and top!='[':
+                    return False
+                if s[i]=='}'and top!='{':
+                    return False
+        return len(a)==0
+
+            
+        
