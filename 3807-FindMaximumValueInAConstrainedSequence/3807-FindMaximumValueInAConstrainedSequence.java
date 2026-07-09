4a2c39246e0bbd2c9da0a559e9b6bec8d97d66f5
Ashwath <achukavii361@gmail.com>
2026-07-09 15:06:44 +0530

    Create 3807-FindMaximumValueInAConstrainedSequence

---
+++ 3807-FindMaximumValueInAConstrainedSequence/3807-FindMaximumValueInAConstrainedSequence.java
@@ -0,0 +1,26 @@
+// Last updated: 7/9/2026, 3:06:43 PM
+class Solution {
+    public int findMaxVal(int n, int[][] restrictions, int[] diff) {
+        long INF = (long) 1e18;
+        long[] limit = new long[n];
+        for (int i = 0; i < n; i++) {
+            limit[i] = INF;
+        }
+        limit[0] = 0;
+        for (int[] r : restrictions) {
+            limit[r[0]] = Math.min(limit[r[0]], r[1]);
+        }
+        for (int i = 1; i < n; i++) {
+            limit[i] = Math.min(limit[i], limit[i - 1] + diff[i - 1]);
+        }
+        for (int i = n - 2; i >= 0; i--) {
+            limit[i] = Math.min(limit[i], limit[i + 1] + diff[i]);
+        }
+        long ans = 0;
+        for (int i = 0; i < n; i++) {
+            ans = Math.max(ans, limit[i]);
+        }
+        return (int) ans;
+    }
+}
+
