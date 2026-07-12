2f1f2bc4685168293d8981269d5ba4219bec3fa6
Ashwath <achukavii361@gmail.com>
2026-07-12 08:53:43 +0530

    Create

---
+++ problem-1783826623107/version/python/problem-1783826623107_20260712_085343.py
@@ -0,0 +1,16 @@
+# Last updated: 7/12/2026, 8:53:43 AM
+1class Solution(object):
+2    def minimumCost(self, nums, k):
+3        q = 10**9 + 7
+4        a = k
+5        b = 0
+6        c = 0
+7        for d in nums:
+8            if a < d:
+9                e = (d - a + k - 1) // k
+10                c += e * (2 * b + e + 1) // 2
+11                b += e
+12                a += e * k
+13            a -= d
+14
+15        return c % q
