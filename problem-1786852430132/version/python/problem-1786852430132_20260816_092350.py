74e5b7f52d86064fe0890e3b902dd2b613ea4d9b
Ashwath <achukavii361@gmail.com>
2026-08-16 09:23:51 +0530

    Create

---
+++ problem-1786852430132/version/python/problem-1786852430132_20260816_092350.py
@@ -0,0 +1,13 @@
+# Last updated: 8/16/2026, 9:23:50 AM
+1class Solution(object):
+2    def minPenalty(self, period, lights, arrivalTime):
+3        a = max(lights)
+4        b = 0
+5        for c in arrivalTime:
+6            d = c % period
+7            if d < a:
+8                continue
+9            e = period - d
+10            if e > b:
+11                b = e
+12        return b
